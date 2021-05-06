4c7b2bf3ada8c927ed5bc5bbb7980642bb14d36c
Alex Hughes <alexhughes@AlexMBPro.local>
2021-05-06 14:09:10 -0500

    feat: add Biology Today article

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import AutoReview from './components/autoReview'
+
+import BiologyToday from './components/biologyToday'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
           My really cool blog site
         </p>
       </header>
-      <AutoReview/>
+      <BiologyToday/>
     </div>
   );
 }
